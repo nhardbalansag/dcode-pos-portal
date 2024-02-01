74c1549033008ef4c4636ff5983b3b6eda26468a
nhardbalansag <nhardbalansag@gmail.com>
2024-02-01 10:59:17 +0800

    add update for ui

--- src/pages/main/dashboard.js
+++ src/pages/main/dashboard.js
@@ -22,67 +22,65 @@ const DashboardPage = () => {
   const data = [12, 19, 3, 700, 1245, 5000, 200, 100, 579, 690, 356, 60]
 
   return (
-    <div>
-      <PageWrapper heading='Dashboard' caption="Here's whats happening with your stores today.">
-        <div className="grid grid-flow-col grid-rows-4 my-5 md:justify-between justify-evenly sm:grid-rows-2 md:grid-rows-1">
-          
-          <DashboardStatusCard 
-          statusTitle='Total Sales' 
-          statusValue='400' 
-          isPositive={true} 
-          value='400' 
-          percentage='22' 
-          icon={<IconPriceTag/>}/>
+    <PageWrapper heading='Dashboard' caption="Here's whats happening with your stores today.">
+      <div className="grid grid-flow-col grid-rows-4 my-5 md:justify-between justify-evenly sm:grid-rows-2 md:grid-rows-1">
+        
+        <DashboardStatusCard 
+        statusTitle='Total Sales' 
+        statusValue='400' 
+        isPositive={true} 
+        value='400' 
+        percentage='22' 
+        icon={<IconPriceTag/>}/>
 
-          <DashboardStatusCard 
-          statusTitle='Total Orders' 
-          statusValue='26K' 
-          isPositive={false} 
-          value='90' 
-          percentage='14' 
-          icon={<IconPushCart/>}/>
+        <DashboardStatusCard 
+        statusTitle='Total Orders' 
+        statusValue='26K' 
+        isPositive={false} 
+        value='90' 
+        percentage='14' 
+        icon={<IconPushCart/>}/>
 
-          <DashboardStatusCard 
-          statusTitle='New Sales' 
-          statusValue='8K' 
-          isPositive={false} 
-          value='90' 
-          percentage='14' 
-          icon={<IconReceipt/>}/>
+        <DashboardStatusCard 
+        statusTitle='New Sales' 
+        statusValue='8K' 
+        isPositive={false} 
+        value='90' 
+        percentage='14' 
+        icon={<IconReceipt/>}/>
 
-          <DashboardStatusCard 
-          statusTitle='Total Products' 
-          statusValue='20' 
-          isPositive={false} 
-          value='90' 
-          percentage='14' 
-          icon={<IconShoppingBag classname='w-8 h-8'/>}/>
+        <DashboardStatusCard 
+        statusTitle='Total Products' 
+        statusValue='20' 
+        isPositive={false} 
+        value='90' 
+        percentage='14' 
+        icon={<IconShoppingBag classname='w-8 h-8'/>}/>
 
-          <DashboardStatusCard 
-          statusTitle='Total Stores' 
-          statusValue='3' 
-          isPositive={false} 
-          value='90' 
-          percentage='14' 
-          icon={<IconStore classname='w-8 h-8'/>}/>
+        <DashboardStatusCard 
+        statusTitle='Total Stores' 
+        statusValue='3' 
+        isPositive={false} 
+        value='90' 
+        percentage='14' 
+        icon={<IconStore classname='w-8 h-8'/>}/>
 
-        </div>
+      </div>
 
-        <div className='flex flex-row items-stretch my-5'>
-          <CardComp titleClassname='text-2xl font-bold tracking-tight text-dark h-1/5' title={'Sales Overview'} width='w-full shadow-lg p-3'>
-            <LineChart data={data}/>
-          </CardComp>
-          
-          <div className='w-1/5 p-3 ml-3 shadow-lg'>
-            <h2 className="text-2xl font-bold tracking-tight text-dark h-1/5">Ledgers</h2>
-            <DonutChart/>
-          </div>
-        </div>
-        <CardComp titleClassname='text-2xl font-bold tracking-tight text-dark h-1/5' title={'Top Sales'} width='w-full shadow-lg my-5'>
-          <DaisyTable/>
+      <div className='flex flex-row items-stretch my-5'>
+        <CardComp titleClassname='text-2xl font-bold tracking-tight text-dark h-1/5' title={'Sales Overview'} width='w-full shadow-lg p-3'>
+          <LineChart data={data}/>
         </CardComp>
-      </PageWrapper>
-    </div>
+        
+        <div className='w-1/5 p-3 ml-3 shadow-lg'>
+          <h2 className="text-2xl font-bold tracking-tight text-dark h-1/5">Ledgers</h2>
+          <DonutChart/>
+        </div>
+      </div>
+      <CardComp titleClassname='text-2xl font-bold tracking-tight text-dark h-1/5' title={'Top Sales'} width='w-full shadow-lg my-5'>
+        <DaisyTable/>
+      </CardComp>
+    </PageWrapper>
   )
 }
 
--- src/pages/main/transaction.js
+++ src/pages/main/transaction.js
@@ -5,56 +5,43 @@ import { Link } from "react-router-dom";
 import {
   CardComp,
   ButtonComp,
-  DonutChart,
-  DaisyTable
+  PageHeaderTitle,
+  DaisyTable,
+  PageWrapper
 } from '../../components/_index'
 
 import {
   IconTableList,
-  IconMinusSwapOff,
   IconTrashCan,
   IconEdit
 } from '../../icons/_index'
 
 const TransactionPage = () => {
 
-  const _title = () =>{
-    return(
-      <div className='flex flex-row items-center justify-between w-full'>
-        <div className='flex flex-row items-center'>
-          <IconTableList/> 
-          <p className='ml-2'>Transaction List</p>
-        </div>
-      </div>
-    )
-  }
-
   return (
-    <div>
-      <div className="flex flex-row justify-between mx-auto my-5 lg:mx-0">
-          <div>
-            <h2 className="text-3xl font-bold tracking-tight text-dark">Transactions</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-400">Manage all store transactions</p>
-          </div>
-        </div>
-        
-        <div className='p-3 my-5 shadow-lg'>
-          <CardComp 
-            width='w-full' 
-            title={_title()} 
-            children={
-              <DaisyTable 
-                enableButton={true} 
-                actionContent={
-                  <>
-                    <ButtonComp title={<IconEdit classname='text-white'/>} className='bg-blue-700'/>
-                    <ButtonComp title={<IconTrashCan classname='text-white'/>} className='bg-red-700 '/>
-                  </>
-                }/>
-              }
-          />
-        </div>
-    </div>
+    <PageWrapper heading='Transactions' caption="Manage all store transactions">
+      <div className='p-3 my-5 shadow-lg'>
+        <CardComp 
+          width='w-full' 
+          title={
+            <PageHeaderTitle 
+            icon={<IconTableList/> }
+            title='Transaction List'
+            />
+          } 
+          children={
+            <DaisyTable 
+              enableButton={true} 
+              actionContent={
+                <>
+                  <ButtonComp title={<IconEdit classname='text-white'/>} className='bg-blue-700'/>
+                  <ButtonComp title={<IconTrashCan classname='text-white'/>} className='bg-red-700 '/>
+                </>
+              }/>
+            }
+        />
+      </div>
+    </PageWrapper>
   )
 }
 
